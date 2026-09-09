8e440e878c48f54cc2c2bdb4085a8a2e229fa19a
Shubham-2122 <shubhamjadav.tops@gmail.com>
2026-09-09 17:56:31 +0530

    text file

---
+++ C/01_intro/03_student_list.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	/* mutli line comment
+	printf("----Student List ----");
+	printf("\n1.Meet");
+	printf("\n2.shubham");
+	printf("\n3.dishant");
+	printf("\n4.jatin");
+	*/
+	
+	printf("----Student List ----\n1.shubham\n2.meet\n3.jatin\n4.sujal");
+	
+	getch();
+}
